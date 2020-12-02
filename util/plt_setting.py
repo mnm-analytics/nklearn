333b1cadd49b63bdbdbd121b814ade03d19ada49
k.nishimura <mnm.signate@gmail.com>
2020-12-02 16:48:50 +0900

    Create plt_setting.py

---
+++ util/plt_setting.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+plt.style.use("ggplot")
+plt.rcParams["figure.figsize"] = (16,10)
+plt.rcParams["font.fontsize"] = 12
